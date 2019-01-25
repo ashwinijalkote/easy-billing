afc268fb423e88c98342970618f74b80f9305858
ashwini <cs.ashwini.jalkote@gmail.com>
2019-01-25 23:16:34 +0530

    webpack config for bundling

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    mode:'development',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-react"]
+                        }
+                    }
+            ]
+        }]
+    }
+
+}

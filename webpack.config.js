9d29182dcd20af0ff228a1d127460d1dadbc756b
ashwini <cs.ashwini.jalkote@gmail.com>
2019-01-30 16:23:57 +0530

    added api server

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,35 @@ const path = require('path');
 
 module.exports = {
     entry: './src/app.js',
-    mode:'development',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, './build'),
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: [
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
                     {
                         loader: 'babel-loader',
                         options: {
                             presets: ["@babel/preset-react"]
                         }
                     }
-            ]
-        }]
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {name:'./index.html'}
+                    }
+                ]
+            }
+        ]
     }
 
 }
